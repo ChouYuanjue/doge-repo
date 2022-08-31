3ff0afe18f2bbf4b7f35c803644839514369f12f
Runnel Zhang <2700074128@qq.com>
2022-08-31 09:15:51 +0800

    vendor(v3): import the XXYan plugin sources

---
+++ doge-v3/mirai-jvm/yan/commands/YanQueryCommands.kt
@@ -0,0 +1,43 @@
+package com.github.commands
+
+import com.github.XXYan
+import com.github.YanConfig
+import com.github.YanData
+import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.CompositeCommand
+
+object YanQueryCommands : CompositeCommand(
+    XXYan,
+    "yan",
+    parentPermission = XXYan.permission
+) {
+    @SubCommand
+    suspend fun CommandSender.length(userId: Long) {
+        val sequence = YanData.getSequence(userId)
+        this.sendMessage("目前该用户的yan数量为:${sequence.rowSet.size()}")
+    }
+
+    @SubCommand
+    suspend fun CommandSender.unsetYan(name: String) {
+        val value = YanConfig.cares.remove(name)
+        this.sendMessage("已成功移除${name} -> $value")
+    }
+
+    @SubCommand
+    suspend fun CommandSender.updateAllDataVersion() {
+        val messages = YanConfig.cares.values.map {
+            YanData.updateDataVersion(it)
+        }.toList()
+        this.sendMessage("操作成功: $messages")
+    }
+
+    @SubCommand
+    suspend fun CommandSender.stars() {
+        val sequence = YanConfig.cares
+        val ids = sequence.values.toSet()
+        this.sendMessage("目前关注的人有:\n${ids.joinToString("\n")}")
+    }
+
+
+}
+
